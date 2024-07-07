5f2c070f9b7143558fcdb67deba16656e5b2382f
Kartik <kartik_a@mt.iitr.ac.in>
2024-07-07 15:09:39 +0530

    changes

---
+++ prisma/migrations/20240707023929_added_unanswered_column/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Message" ADD COLUMN     "unanswered" BOOLEAN NOT NULL DEFAULT false;
--- src/routes/v1/admin.ts
+++ src/routes/v1/admin.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { db } from '../../common/db';
+import { start } from 'repl';
 const express = require('express');
 const router = Router();
 
@@ -162,22 +163,22 @@ router.post('/recent-chats', async (req, res) => {
     try {
         const tickets = await db.aIConversationTicket.findMany({
             where: {
-              shopDomain: shopDomain,
+                shopDomain: shopDomain,
             },
             orderBy: {
-              createdAt: 'desc',
+                createdAt: 'desc',
             },
             take: 3,
             include: {
-              messages: {
-                take: 2, 
-              },
+                messages: {
+                    take: 2,
+                },
             },
-          });
-          const result = tickets.map(ticket => ({
+        });
+        const result = tickets.map(ticket => ({
             id: ticket.id,
             messages: ticket.messages,
-          }));
+        }));
         console.log(result)
         res.json({
             result
@@ -189,5 +190,194 @@ router.post('/recent-chats', async (req, res) => {
         })
     }
 })
+
+router.post('/unanswered', async (req, res) => {
+    console.log("fetching for unanswered messages")
+    const { shopDomain } = req.body
+    const { startTime } = req.body
+    const { endTime } = req.body
+    console.log(shopDomain)
+    try {
+        const count = await db.message.count({
+            where: {
+                unanswered: true,
+                createdAt: {
+                    gte: startTime,
+                    lte: endTime
+                },
+                sender: 'ai',
+                aiconversationticket: {
+                    shopDomain: shopDomain
+                }
+            },
+
+        });
+        res.json({
+            count
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(404).json({
+            "message": "Technical Error"
+        })
+    }
+})
+router.post('/answered', async (req, res) => {
+    console.log("fetching for unanswered messages")
+    const { shopDomain } = req.body
+    const { startTime } = req.body
+    const { endTime } = req.body
+    console.log(shopDomain)
+    try {
+        const count = await db.message.count({
+            where: {
+                unanswered: false,
+                createdAt: {
+                    gte: startTime,
+                    lte: endTime
+                },
+                sender: 'ai',
+                aiconversationticket: {
+                    shopDomain: shopDomain
+                }
+            },
+
+        });
+        res.json({
+            count
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(404).json({
+            "message": "Technical Error"
+        })
+    }
+})
+
+router.post('/count-ai-tickets', async (req, res) => {
+    console.log("fetching for analytics total tickets")
+    const { startTime } = req.body
+    const { endTime } = req.body
+    const { shopDomain } = req.body
+    console.log(startTime)
+    console.log(endTime)
+    console.log(shopDomain)
+    try {
+        const count = await db.aIConversationTicket.count({
+            where: {
+                shopDomain: shopDomain,
+                createdAt: {
+                    gte: startTime,
+                    lte: endTime,
+                },
+            },
+        })
+        // console.log(count)
+        res.json({
+            count
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(404).json({
+            "message": "Technical Error"
+        })
+    }
+})
+
+router.post('/avg-session', async (req, res) => {
+    console.log("fetching for analytics total tickets")
+    const { startTime } = req.body
+    const { endTime } = req.body
+    const { shopDomain } = req.body
+    console.log(startTime)
+    console.log(endTime)
+    console.log(shopDomain)
+    try {
+        const tickets = await db.aIConversationTicket.findMany({
+            where: {
+                shopDomain: shopDomain,
+                createdAt: {
+                    gte: startTime,
+                    lte: endTime,
+                },
+            },
+            include: {
+                messages: {
+                    orderBy: {
+                        createdAt: 'asc',
+                    },
+                },
+            },
+        });
+        let totalDifference = 0;
+        let totalTickets = 0;
+
+        tickets.forEach(ticket => {
+            if (ticket.messages.length > 1) {
+                const firstMessageTime = new Date(ticket.messages[0].createdAt).getTime();
+                const lastMessageTime = new Date(ticket.messages[ticket.messages.length - 1].createdAt).getTime();
+                const differenceInSeconds = (lastMessageTime - firstMessageTime) / 1000;
+                totalDifference += differenceInSeconds;
+                totalTickets += 1;
+            }
+        });
+
+        const avgSession = totalTickets > 0 ? totalDifference / totalTickets : 0;
+        res.json({
+            avgSession
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(404).json({
+            "message": "Technical Error"
+        })
+    }
+})
+
+router.post('/transfer-rate', async (req, res) => {
+    console.log("fetching for analytics transfer rate")
+    const { startTime } = req.body
+    const { endTime } = req.body
+    const { shopDomain } = req.body
+    console.log(startTime)
+    console.log(endTime)
+    console.log(shopDomain)
+    try {
+        const totalTickets = await db.aIConversationTicket.count({
+            where: {
+                shopDomain: shopDomain,
+                createdAt: {
+                    gte: startTime,
+                    lte: endTime,
+                },
+            },
+        });
+
+        const escalatedTickets = await db.aIConversationTicket.count({
+            where: {
+                shopDomain: shopDomain,
+                createdAt: {
+                    gte: startTime,
+                    lte: endTime,
+                },
+                AIEscalatedTicket: {
+                    some: {}, // This checks if there is at least one related AIEscalatedTicket
+                },
+            },
+        });
+
+        let ratio = totalTickets > 0 ? escalatedTickets / totalTickets : 0;
+        ratio *= 100;
+        res.json({
+            ratio
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(404).json({
+            "message": "Technical Error"
+        })
+    }
+})
+
 
 module.exports = router;
